06e36732320c398272264e1b9ea9e3f90ff86347
imsaravanakumar <144697260+imsaravanakumar@users.noreply.github.com>
2025-06-26 19:43:25 +0530

    Update Hero.jsx

--- src/componants/Hero.jsx
+++ src/componants/Hero.jsx
@@ -17,11 +17,11 @@ export default function Hero() {
                 bg-clip-text text-transparent mt-5 shadow-current'>{hero.heroname} </h1>
                     <h1 className='text-2xl md:text-4xl py-2'>{hero.developer}</h1>
                     <p className='w-3/4 mx-auto lg:mx-0'>{hero.discription}</p>
-                    <button className='hidden lg:block py-3 px-6 font-medium shadow-btn custom-shadow my-6'><a href="https://drive.google.com/file/d/1nifjxDn70afVpLpyYmlvKlXjnnJwTp3g/view?usp=drivesdk">Download CV</a></button>
+                    <button className='hidden lg:block py-3 px-6 font-medium shadow-btn custom-shadow my-6'><a href="https://drive.google.com/file/d/1owWEDeIozRcomh9H30Ybr5hNPATO_87O/view?usp=drivesdk">Download CV</a></button>
                 </div>               
             </div>
             <img className='heroimg w-1/2 mx-auto lg:w-1/3 xl:w-1/5  my-10 lg:m-0 rounded-full custom-shadow' src={Myimg} alt='Saravana Image' />
-            <button className='block lg:hidden w-1/2 py-3 px-6  my-6 mx-auto font-medium text-nowrap text-white shadow-btn custom-shadow'><a href="https://drive.google.com/file/d/1nifjxDn70afVpLpyYmlvKlXjnnJwTp3g/view?usp=drivesdk">Download CV</a> </button>
+            <button className='block lg:hidden w-1/2 py-3 px-6  my-6 mx-auto font-medium text-nowrap text-white shadow-btn custom-shadow'><a href="https://drive.google.com/file/d/1owWEDeIozRcomh9H30Ybr5hNPATO_87O/view?usp=drivesdk">Download CV</a> </button>
         </section>
     )
 }
